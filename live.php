66638a9fe45a86f9d2d1bd44dafd942a2e488ae7
unknown <rabiul0420@gmail.com>
2022-11-12 16:31:54 +0600

    jjjj

--- live.php
+++ live.php
@@ -30,6 +30,10 @@
     <!--Minify Styles-->
     <!-- <link rel="stylesheet" href="../../dist/css/bundle.min.css"> -->
     <link rel="stylesheet" href="index.css"/>
+
+
+
+
 </head>
 
 <body class="bg-repeat font-family">
@@ -130,7 +134,114 @@
                             </div>
                         </div>
                     </div>
+                    <div class="row">
+                        <div class="MultiCarousel" data-items="1,3,5,6" data-slide="1" id="MultiCarousel"  data-interval="1000">
+                            <div class="MultiCarousel-inner">
+                                <div class="item dcr-411vm1">
+                                    <a href="?filterKeyEvents=false&amp;page=with:block-6335da528f0883d28b5849fc#block-6335da528f0883d28b5849fc" data-link-name="key event card | 0 of 14" class="dcr-1ah36un">
+                                        <time datetime="2022-09-29T18:00:13.000Z" data-relativeformat="med" title="Thursday, 29 September 2022, 18:00 Coordinated Universal Time" class="dcr-bd9v7x">30 Sept 2022</time>
+                                        <div class="dcr-19vcojp">
+                                            Nato warns of ‘determined response’ following ‘deliberate sabotage’ on Nord Stream gas pipelines
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="item dcr-411vm1">
+                                    <a href="?filterKeyEvents=false&amp;page=with:block-6335da528f0883d28b5849fc#block-6335da528f0883d28b5849fc" data-link-name="key event card | 0 of 14" class="dcr-1ah36un">
+                                        <time datetime="2022-09-29T18:00:13.000Z" data-relativeformat="med" title="Thursday, 29 September 2022, 18:00 Coordinated Universal Time" class="dcr-bd9v7x">30 Sept 2022</time>
+                                        <div class="dcr-19vcojp">
+                                            Nato warns of ‘determined response’ following ‘deliberate sabotage’ on Nord Stream gas pipelines
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="item dcr-411vm1">
+                                    <a href="?filterKeyEvents=false&amp;page=with:block-6335da528f0883d28b5849fc#block-6335da528f0883d28b5849fc" data-link-name="key event card | 0 of 14" class="dcr-1ah36un">
+                                        <time datetime="2022-09-29T18:00:13.000Z" data-relativeformat="med" title="Thursday, 29 September 2022, 18:00 Coordinated Universal Time" class="dcr-bd9v7x">30 Sept 2022</time>
+                                        <div class="dcr-19vcojp">
+                                            Nato warns of ‘determined response’ following ‘deliberate sabotage’ on Nord Stream gas pipelines
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="item dcr-411vm1">
+                                    <a href="?filterKeyEvents=false&amp;page=with:block-6335da528f0883d28b5849fc#block-6335da528f0883d28b5849fc" data-link-name="key event card | 0 of 14" class="dcr-1ah36un">
+                                        <time datetime="2022-09-29T18:00:13.000Z" data-relativeformat="med" title="Thursday, 29 September 2022, 18:00 Coordinated Universal Time" class="dcr-bd9v7x">30 Sept 2022</time>
+                                        <div class="dcr-19vcojp">
+                                            Nato warns of ‘determined response’ following ‘deliberate sabotage’ on Nord Stream gas pipelines
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="item dcr-411vm1">
+                                    <a href="?filterKeyEvents=false&amp;page=with:block-6335da528f0883d28b5849fc#block-6335da528f0883d28b5849fc" data-link-name="key event card | 0 of 14" class="dcr-1ah36un">
+                                        <time datetime="2022-09-29T18:00:13.000Z" data-relativeformat="med" title="Thursday, 29 September 2022, 18:00 Coordinated Universal Time" class="dcr-bd9v7x">30 Sept 2022</time>
+                                        <div class="dcr-19vcojp">
+                                            Nato warns of ‘determined response’ following ‘deliberate sabotage’ on Nord Stream gas pipelines
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="item dcr-411vm1">
+                                    <a href="?filterKeyEvents=false&amp;page=with:block-6335da528f0883d28b5849fc#block-6335da528f0883d28b5849fc" data-link-name="key event card | 0 of 14" class="dcr-1ah36un">
+                                        <time datetime="2022-09-29T18:00:13.000Z" data-relativeformat="med" title="Thursday, 29 September 2022, 18:00 Coordinated Universal Time" class="dcr-bd9v7x">30 Sept 2022</time>
+                                        <div class="dcr-19vcojp">
+                                            Nato warns of ‘determined response’ following ‘deliberate sabotage’ on Nord Stream gas pipelines
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="item dcr-411vm1">
+                                    <a href="?filterKeyEvents=false&amp;page=with:block-6335da528f0883d28b5849fc#block-6335da528f0883d28b5849fc" data-link-name="key event card | 0 of 14" class="dcr-1ah36un">
+                                        <time datetime="2022-09-29T18:00:13.000Z" data-relativeformat="med" title="Thursday, 29 September 2022, 18:00 Coordinated Universal Time" class="dcr-bd9v7x">30 Sept 2022</time>
+                                        <div class="dcr-19vcojp">
+                                            Nato warns of ‘determined response’ following ‘deliberate sabotage’ on Nord Stream gas pipelines
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="item dcr-411vm1">
+                                    <a href="?filterKeyEvents=false&amp;page=with:block-6335da528f0883d28b5849fc#block-6335da528f0883d28b5849fc" data-link-name="key event card | 0 of 14" class="dcr-1ah36un">
+                                        <time datetime="2022-09-29T18:00:13.000Z" data-relativeformat="med" title="Thursday, 29 September 2022, 18:00 Coordinated Universal Time" class="dcr-bd9v7x">30 Sept 2022</time>
+                                        <div class="dcr-19vcojp">
+                                            Nato warns of ‘determined response’ following ‘deliberate sabotage’ on Nord Stream gas pipelines
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="item dcr-411vm1">
+                                    <a href="?filterKeyEvents=false&amp;page=with:block-6335da528f0883d28b5849fc#block-6335da528f0883d28b5849fc" data-link-name="key event card | 0 of 14" class="dcr-1ah36un">
+                                        <time datetime="2022-09-29T18:00:13.000Z" data-relativeformat="med" title="Thursday, 29 September 2022, 18:00 Coordinated Universal Time" class="dcr-bd9v7x">30 Sept 2022</time>
+                                        <div class="dcr-19vcojp">
+                                            Nato warns of ‘determined response’ following ‘deliberate sabotage’ on Nord Stream gas pipelines
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="item dcr-411vm1">
+                                    <a href="?filterKeyEvents=false&amp;page=with:block-6335da528f0883d28b5849fc#block-6335da528f0883d28b5849fc" data-link-name="key event card | 0 of 14" class="dcr-1ah36un">
+                                        <time datetime="2022-09-29T18:00:13.000Z" data-relativeformat="med" title="Thursday, 29 September 2022, 18:00 Coordinated Universal Time" class="dcr-bd9v7x">30 Sept 2022</time>
+                                        <div class="dcr-19vcojp">
+                                            Nato warns of ‘determined response’ following ‘deliberate sabotage’ on Nord Stream gas pipelines
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="item dcr-411vm1">
+                                    <a href="?filterKeyEvents=false&amp;page=with:block-6335da528f0883d28b5849fc#block-6335da528f0883d28b5849fc" data-link-name="key event card | 0 of 14" class="dcr-1ah36un">
+                                        <time datetime="2022-09-29T18:00:13.000Z" data-relativeformat="med" title="Thursday, 29 September 2022, 18:00 Coordinated Universal Time" class="dcr-bd9v7x">30 Sept 2022</time>
+                                        <div class="dcr-19vcojp">
+                                            Nato warns of ‘determined response’ following ‘deliberate sabotage’ on Nord Stream gas pipelines
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="item dcr-411vm1">
+                                    <a href="?filterKeyEvents=false&amp;page=with:block-6335da528f0883d28b5849fc#block-6335da528f0883d28b5849fc" data-link-name="key event card | 0 of 14" class="dcr-1ah36un">
+                                        <time datetime="2022-09-29T18:00:13.000Z" data-relativeformat="med" title="Thursday, 29 September 2022, 18:00 Coordinated Universal Time" class="dcr-bd9v7x">30 Sept 2022</time>
+                                        <div class="dcr-19vcojp">
+                                            Nato warns of ‘determined response’ following ‘deliberate sabotage’ on Nord Stream gas pipelines
+                                        </div>
+                                    </a>
+                                </div>
+
+                            </div>
+                            <button class="btn btn-primary leftLst"><</button>
+                            <button class="btn btn-primary rightLst">></button>
+                        </div>
+                    </div>
                 </div>
+
+
 
 
 
@@ -290,6 +401,117 @@
             rtl: true,
             asNavFor: '.flexslider'
         });
+
+
+
+
+        $(document).ready(function () {
+            var itemsMainDiv = ('.MultiCarousel');
+            var itemsDiv = ('.MultiCarousel-inner');
+            var itemWidth = "";
+
+            $('.leftLst, .rightLst').click(function () {
+                var condition = $(this).hasClass("leftLst");
+                if (condition)
+                    click(0, this);
+                else
+                    click(1, this)
+            });
+
+            ResCarouselSize();
+
+
+
+
+            $(window).resize(function () {
+                ResCarouselSize();
+            });
+
+            //this function define the size of the items
+            function ResCarouselSize() {
+                var incno = 0;
+                var dataItems = ("data-items");
+                var itemClass = ('.item');
+                var id = 0;
+                var btnParentSb = '';
+                var itemsSplit = '';
+                var sampwidth = $(itemsMainDiv).width();
+                var bodyWidth = $('body').width();
+                $(itemsDiv).each(function () {
+                    id = id + 1;
+                    var itemNumbers = $(this).find(itemClass).length;
+                    btnParentSb = $(this).parent().attr(dataItems);
+                    itemsSplit = btnParentSb.split(',');
+                    $(this).parent().attr("id", "MultiCarousel" + id);
+
+
+                    if (bodyWidth >= 1200) {
+                        incno = itemsSplit[3];
+                        itemWidth = sampwidth / incno;
+                    }
+                    else if (bodyWidth >= 992) {
+                        incno = itemsSplit[2];
+                        itemWidth = sampwidth / incno;
+                    }
+                    else if (bodyWidth >= 768) {
+                        incno = itemsSplit[1];
+                        itemWidth = sampwidth / incno;
+                    }
+                    else {
+                        incno = itemsSplit[0];
+                        itemWidth = sampwidth / incno;
+                    }
+                    $(this).css({ 'transform': 'translateX(0px)', 'width': itemWidth * itemNumbers });
+                    $(this).find(itemClass).each(function () {
+                        $(this).outerWidth(itemWidth);
+                    });
+
+                    $(".leftLst").addClass("over");
+                    $(".rightLst").removeClass("over");
+
+                });
+            }
+
+
+            //this function used to move the items
+            function ResCarousel(e, el, s) {
+                var leftBtn = ('.leftLst');
+                var rightBtn = ('.rightLst');
+                var translateXval = '';
+                var divStyle = $(el + ' ' + itemsDiv).css('transform');
+                var values = divStyle.match(/-?[\d\.]+/g);
+                var xds = Math.abs(values[4]);
+                if (e == 0) {
+                    translateXval = parseInt(xds) - parseInt(itemWidth * s);
+                    $(el + ' ' + rightBtn).removeClass("over");
+
+                    if (translateXval <= itemWidth / 2) {
+                        translateXval = 0;
+                        $(el + ' ' + leftBtn).addClass("over");
+                    }
+                }
+                else if (e == 1) {
+                    var itemsCondition = $(el).find(itemsDiv).width() - $(el).width();
+                    translateXval = parseInt(xds) + parseInt(itemWidth * s);
+                    $(el + ' ' + leftBtn).removeClass("over");
+
+                    if (translateXval >= itemsCondition - itemWidth / 2) {
+                        translateXval = itemsCondition;
+                        $(el + ' ' + rightBtn).addClass("over");
+                    }
+                }
+                $(el + ' ' + itemsDiv).css('transform', 'translateX(' + -translateXval + 'px)');
+            }
+
+            //It is used to get some elements from btn
+            function click(ell, ee) {
+                var Parent = "#" + $(ee).parent().attr("id");
+                var slide = $(Parent).attr("data-slide");
+                ResCarousel(ell, Parent, slide);
+            }
+
+        });
+
     });
 </script>
 
